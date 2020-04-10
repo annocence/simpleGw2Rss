c929206c3929a736a95121e207627dc21d3b8dcf
marcus <marcusla@gmx.de>
2020-04-10 19:10:52 +0200

    change path

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -4,7 +4,7 @@ import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
     handlers {
-        get("playGw2Today") {
+        get("playGw2") {
             def iterator = Stream.generate({ -> "Not today" }).iterator()
             render iterator.next()
         }
